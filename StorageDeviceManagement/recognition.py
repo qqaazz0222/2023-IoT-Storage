2615df3b8182868bdafedf4dd29769de8d4c7a12
qqaazz0222 <qqaazz0222@naver.com>
2023-10-24 19:01:25 +0900

    feat : test #3

--- StorageDeviceManagement/recognition.py
+++ StorageDeviceManagement/recognition.py
@@ -1,11 +1,31 @@
 import os
+import sys
 
-diskInfo = os.statvfs('/')
-used = diskInfo.f_bsize * (diskInfo.f_blocks - diskInfo.f_bavail)
-free = diskInfo.f_bsize * diskInfo.f_bavail
-total = diskInfo.f_bsize * diskInfo.f_blocks
-print(used)
-print(free)
-print(total)
+import psutil
+from psutil._common import bytes2human
 
-os.path.isdir() 
+
+def main():
+    templ = "%-17s %8s %8s %8s %5s%% %9s  %s"
+    print(templ % ("Device", "Total", "Used", "Free", "Use ", "Type",
+                   "Mount"))
+    for part in psutil.disk_partitions(all=False):
+        if os.name == 'nt':
+            if 'cdrom' in part.opts or part.fstype == '':
+                # skip cd-rom drives with no disk in it; they may raise
+                # ENOENT, pop-up a Windows GUI error for a non-ready
+                # partition or just hang.
+                continue
+        usage = psutil.disk_usage(part.mountpoint)
+        print(templ % (
+            part.device,
+            bytes2human(usage.total),
+            bytes2human(usage.used),
+            bytes2human(usage.free),
+            int(usage.percent),
+            part.fstype,
+            part.mountpoint))
+
+
+if __name__ == '__main__':
+    sys.exit(main())
--- SystemManagement/cpuInfo.py
+++ SystemManagement/cpuInfo.py
@@ -1,9 +1,13 @@
 import psutil
 
-cpu_percent = psutil.cpu_percent()
-cpu_times_percent = psutil.cpu_times_percent()
-cpu_idel = psutil.cpu_idle()
+cpu = psutil.cpu_times_percent()
+usage = psutil.cpu_percent(interval=None, percpu=False)
+usage_per_cpu = psutil.cpu_percent(interval=None, percpu=True)
+idel = cpu.idle
+logi_core = psutil.cpu_count()
+pysi_core = psutil.cpu_count(logical=False)
+times = psutil.cpu_times(percpu=False)
 
-print(cpu_percent)
-print(cpu_times_percent)
-print(cpu_idel)
+if __name__ == "__main__":
+    print(
+        f"CPU 사용율 : {usage}%, 유휴 CPU : {idel}%, 논리적 코어 수 : {logi_core}, 물리적 코어 수 : {pysi_core}, 동작 시간 : {times.user + times.system}초")
