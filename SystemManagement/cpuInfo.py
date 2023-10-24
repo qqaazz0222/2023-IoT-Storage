3a0e1711c42e24639720af6e4cdbf9d280f42fea
qqaazz0222 <qqaazz0222@naver.com>
2023-10-24 18:47:54 +0900

    feat : cpu test #2

--- SystemManagement/cpuInfo.py
+++ SystemManagement/cpuInfo.py
@@ -1,5 +1,9 @@
 import psutil
 
 cpu_percent = psutil.cpu_percent()
+cpu_times_percent = psutil.cpu_times_percent()
+cpu_idel = psutil.cpu_idle()
 
 print(cpu_percent)
+print(cpu_times_percent)
+print(cpu_idel)
